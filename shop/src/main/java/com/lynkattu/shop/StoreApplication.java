dc46711977168cee5eb797def6386157dfb737dd
Lynkattu <121508843+Lynkattu@users.noreply.github.com>
2026-07-26 22:51:11 +0300

    Add files via upload

---
+++ shop/src/main/java/com/lynkattu/shop/StoreApplication.java
@@ -0,0 +1,15 @@
+package com.lynkattu.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// command for running app: ./mvnw spring-boot:run
+
+@SpringBootApplication
+public class StoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApplication.class, args);
+	}
+
+}
